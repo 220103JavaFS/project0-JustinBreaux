1a38f3d83801339f17cba923da6881daaaa0ce8a
JustinBreaux <96553121+JustinBreaux@users.noreply.github.com>
2022-01-09 19:11:31 -0500

    fixed models package, added Driver, tested logback

---
+++ src/main/java/com/revature/Driver.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Driver {
+
+    private static Logger log = LoggerFactory.getLogger(Driver.class);
+
+    public static void main(String[] args) {
+        log.debug("test");
+
+    }
+}
--- src/main/java/com/revature/com/revature/models/User.java
+++
@@ -1,4 +0,0 @@
-package com.revature.com.revature.models;
-
-public class User {
-}
